638207beca61e5935339c1a3c5a656e6fb80c61c
peterslopes <45262485+peterslopes@users.noreply.github.com>
2021-10-22 15:03:47 -0300

    Update exos.py

--- napalm_exos/exos.py
+++ napalm_exos/exos.py
@@ -69,7 +69,7 @@ class ExosDriver(NetworkDriver):
             'startup': '',
             'running': '',
         }
-        configs['running'] = self.device.send_command('show configuration | e sysName')
+        configs['running'] = self.device.send_command('show configuration')# | e sysName')
 
         return configs
 
