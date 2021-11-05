70c07df3c7ccf0b0bdc18b01d81c908c3de165b2
peterslopes <45262485+peterslopes@users.noreply.github.com>
2021-11-05 12:39:40 -0300

    Update exos.py

--- napalm_exos/exos.py
+++ napalm_exos/exos.py
@@ -71,7 +71,8 @@ class ExosDriver(NetworkDriver):
         }
         command1 = self.device.send_command('show configuration | i sysName')
         command2 = self.device.send_command('show configuration | e sysName')
-        configs['running'] = command1 + command2
+        configs['running'] = command1 + '\n' + command2
+        
         return configs
     
     def get_optics(self, interface=None):
